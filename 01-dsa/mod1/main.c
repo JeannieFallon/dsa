4d50dd61b34a8d157c1710f54f9cd2492a719ca3
Jeannie Fallon <jeanniefallon@gmail.com>
2023-07-11 12:01:15 -0400

    add user input loop

--- 01-dsa/mod1/main.c
+++ 01-dsa/mod1/main.c
@@ -3,8 +3,33 @@
 
 /* Module 1: ArrayList */
 
+char ACTIONS[] = "0\tQuit\n1\tPrint array\n";
+
 int main(int argc, char **argv) {
-    printf("Hello, Module1\n");
-    sayfoo();
+    printf("*** Module 1: ArrayList ***\n");
+
+    int ans = -1;
+
+    do {
+        printf("\nChoose an action:\n");
+        printf("-----\n");
+        printf("%s", ACTIONS);
+        printf("-----\n");
+
+        scanf("%d", &ans);
+
+        switch(ans) {
+            case 0:
+                printf("Goodbye!\n");
+                break;
+            case 1:
+                sayfoo();
+                break;
+            default:
+                printf("Action not availble\n");
+        }
+
+    } while (ans != 0);
+
     return 0;
 }
