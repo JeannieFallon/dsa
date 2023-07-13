daf80782370616e0fb7f2e6e64503715a28ce422
Jeannie Fallon <jeanniefallon@gmail.com>
2023-07-13 18:51:31 -0400

    start add to front

--- module1/mod1.c
+++ module1/mod1.c
@@ -12,8 +12,18 @@ void init(struct array_list* arrlst) {
 }
 
 void print_arr(struct array_list* arrlst) {
-    printf(">>> Current array:\n");
+    printf(">>> Current array:");
     for (int i = 0; i < INIT_CAP; i++) {
         printf("%d ", arrlst->arr[i]);
     }
+    printf("\n");
+}
+
+void add_to_front(struct array_list* arrlst) {
+    int num;
+
+    printf(">>> Enter number to add to front of array:\n");
+    scanf("%d", &num);
+
+    printf(">>> Add %d to front of array //TODO\n", num);
 }
