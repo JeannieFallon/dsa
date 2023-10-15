df6b7c73e2f5779db2178d56c723dadf0d3f75c9
Jeannie Fallon <jeannie.fallon@kyrus-tech.com>
2023-10-15 14:59:34 -0400

    stub out ux

--- module2/main.c
+++ module2/main.c
@@ -1,8 +1,36 @@
+/* Module 2: LinkedList */
 #include <stdio.h>
 
-/* Module 2 */
+char ACTIONS[] =
+    "0\tQuit\n"
+    "1\tPrint list\n";
 
 int main(int argc, char **argv) {
-    printf("Hello, Module2\n");
+    printf("*** Module 2: LinkedList (integers only) ***\n");
+
+    // Loop over user input to control ArrayList
+    int ans = -1;
+    do {
+        printf("\nChoose an action:\n");
+        printf("-----\n");
+        printf("%s", ACTIONS);
+        printf("-----\n");
+
+        scanf("%d", &ans);
+
+        switch(ans) {
+            case 0:
+                printf("\nGoodbye!\n");
+                break;
+            case 1:
+                //TODO
+                printf("\nTODO: print list\n");
+                break;
+            default:
+                printf("Action not availble\n");
+        }
+
+    } while (ans != 0);
+
     return 0;
 }
