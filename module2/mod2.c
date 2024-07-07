3a5786accb28465a2a7473ac7e4018afe5d11e9e
Jeannie Fallon <jeanniefallon@gmail.com>
2024-07-07 11:13:58 -0400

    implement add_to_back and handle list size 0

--- module2/mod2.c
+++ module2/mod2.c
@@ -22,21 +22,22 @@ void print_sll(struct singly_linked_list* sll) {
 void add_to_front(struct singly_linked_list* sll) {
     int val;
 
-    printf("\nEnter number to add to front of array:\n");
+    printf("\nEnter number to add to FRONT of array:\n");
     scanf("%d", &val);
 
     // Create new node
     struct sll_node* new_node = malloc(sizeof(struct sll_node));
     new_node->value = val;
 
-    // If HEAD exists, set as NEXT on new node
-    if (NULL != sll->head) {
+    if (sll->size > 0) {
+        // Set NEXT on new node to point to previous HEAD
         new_node->next = sll->head;
     } else {
-        new_node->next = NULL;
+        // If list is empty, need to set new node as TAIL also
+        sll->tail = new_node;
     }
 
-    // Re-assign HEAD to new node
+    // Re-assign HEAD to new node and increment list size
     sll->head = new_node;
     sll->size++;
 
@@ -44,7 +45,28 @@ void add_to_front(struct singly_linked_list* sll) {
 }
 
 void add_to_back(struct singly_linked_list* sll) {
-    printf("\nTODO\n");
+    int val;
+
+    printf("\nEnter number to add to BACK of array:\n");
+    scanf("%d", &val);
+
+    // Create new node
+    struct sll_node* new_node = malloc(sizeof(struct sll_node));
+    new_node->value = val;
+
+    if (sll->size > 0) {
+        // Set NEXT on previous TAIL to new node
+        sll->tail->next = new_node;
+    } else {
+        // If list is empty, need to set new node as HEAD also
+        sll->head = new_node;
+    }
+
+    // Re-assign TAIL to new node and increment list size
+    sll->tail = new_node;
+    sll->size++;
+
+    print_sll(sll);
 }
 
 void remove_from_front(struct singly_linked_list* sll) {
